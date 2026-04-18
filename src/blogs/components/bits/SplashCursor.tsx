5a12b833a01501c822b053fa8467b4c74d37ab81
metachasm-beep <metachasm@gmail.com>
2026-04-19 01:19:15 +0530

    fix: resolve bug_type 313 by reducing CSS blur complexity and lowering WebGL shader precision

--- src/blogs/components/bits/SplashCursor.tsx
+++ src/blogs/components/bits/SplashCursor.tsx
@@ -395,22 +395,15 @@ export default function SplashCursor({
     );
 
     const displayShaderSource = `
-      precision highp float;
-      precision highp sampler2D;
+      precision mediump float;
+      precision mediump sampler2D;
       varying vec2 vUv;
       varying vec2 vL;
       varying vec2 vR;
       varying vec2 vT;
       varying vec2 vB;
       uniform sampler2D uTexture;
-      uniform sampler2D uDithering;
-      uniform vec2 ditherScale;
       uniform vec2 texelSize;
-
-      vec3 linearToGamma (vec3 color) {
-          color = max(color, vec3(0));
-          return max(1.055 * pow(color, vec3(0.416666667)) - 0.055, vec3(0));
-      }
 
       void main () {
           vec3 c = texture2D(uTexture, vUv).rgb;
